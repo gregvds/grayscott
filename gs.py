71290e0b751e9869ccd1ec6379e2a6fafdc2660f
Grégoire Vandenschrick <gregvds0@gmail.com>
2022-01-04 13:38:07 +0100

    reorganisation of code

--- gs.py
+++ gs.py
@@ -72,290 +72,17 @@ import argparse
 import textwrap
 
 import numpy as np
-from numpy.random import rand
 
-import vispy.color as color
 from vispy import app, gloo
 from vispy.gloo import gl
 
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from shaders import (vertex_shader, compute_fragment, render_hs_fragment,
+                     lines_vertex, lines_fragment)
 
-from shaders import vertex_shader, compute_fragment, render_hs_fragment
-from shaders import lines_vertex, lines_fragment
-from systems import pearson, PearsonGrid, PearsonCurve, PearsonCurve2
+from systems import (pearson, PearsonGrid, PearsonCurve, PearsonCurve2)
 
-
-def get_colormap(name, size=256):
-    vispy_cmaps = color.get_colormaps()
-    if name in vispy_cmaps:
-        cmap = color.get_colormap(name)
-    else:
-        try:
-            mpl_cmap = plt.get_cmap(name)
-        except AttributeError:
-            raise KeyError(
-                f'Colormap "{name}" not found in either vispy '
-                'or matplotlib.'
-            )
-        mpl_colors = mpl_cmap(np.linspace(0, 1, size))
-        cmap = color.Colormap(mpl_colors)
-    return cmap
-
-
-def invertCmapName(cmap):
-    # a very crude reversing name method for Matplotlib colormap inversion
-    if '_r' in cmap:
-        return(cmap.split('_')[0])
-    else:
-        return(cmap+'_r')
-
-
-def createAndRegisterCmap(name, cmapNameList, proportions=None):
-    # routine to create custom Matplotlib colormap and its reversed version
-    newColors = []
-    newColors_r = []
-    if proportions and len(proportions) != len(cmapNameList):
-        raise KeyError(
-            f'cmapNameList and proportions for Colormap {name}'
-            'have not the same length.')
-    if proportions is None:
-        proportions = 32 * np.ones(1, len(cmapNameList))
-
-    for cmapName, proportion in zip(cmapNameList, proportions):
-        newColors.append(cm.get_cmap(cmapName, proportion)
-                         (np.linspace(0, 1, proportion)))
-    newcmp = ListedColormap(np.vstack(newColors), name=name)
-    cmapNameList.reverse()
-    proportions.reverse()
-    for cmapName, proportion in zip(cmapNameList, proportions):
-        newColors_r.append(cm.get_cmap(invertCmapName(
-            cmapName), proportion)(np.linspace(0, 1, proportion)))
-    newcmp_r = ListedColormap(np.vstack(newColors_r),
-                              name=invertCmapName(name))
-    cm.register_cmap(name=name, cmap=newcmp)
-    cm.register_cmap(name=invertCmapName(name), cmap=newcmp_r)
-
-
-def createAndRegisterLinearSegmentedCmap(name, colors, nodes=None):
-    # routine to create custom Matplotlib colormap and its reversed version
-    if nodes and len(nodes) != len(colors):
-        raise KeyError(
-            f'colors and nodes for Colormap {name}'
-            'have not the same length.')
-    if nodes is None:
-        nodes = np.linspace(0, 1, len(colors))
-
-    newcmp = LinearSegmentedColormap.from_list(name, list(zip(nodes, colors)))
-    colors.reverse()
-    nodes.reverse()
-    nodes = [1-x for x in nodes]
-    newcmp_r = LinearSegmentedColormap.from_list(invertCmapName(name), list(zip(nodes, colors)))
-    cm.register_cmap(name=name, cmap=newcmp)
-    cm.register_cmap(name=invertCmapName(name), cmap=newcmp_r)
-
-
-def createColormaps():
-    # definition of some custom colormaps
-    createAndRegisterCmap(
-        'malmo', [
-            'bone_r',
-            'Blues_r',
-            'GnBu_r',
-            'Greens'],
-        proportions=[512, 128, 512, 128])
-    createAndRegisterCmap(
-        'Boston', [
-            'bone_r',
-            'gray',
-            'pink_r',
-            'YlOrBr_r'],
-        proportions=[960, 160, 160, 960])
-    createAndRegisterLinearSegmentedCmap(
-        'detroit', [
-            'white',
-            'lightblue',
-            'cadetblue',
-            'black',
-            'cadetblue',
-            'lightblue',
-            'white'
-        ],
-        nodes=[0.0, 0.4, 0.47, 0.5, 0.53, 0.6, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'antidetroit', [
-            'black',
-            'cadetblue',
-            'lightblue',
-            'white',
-            'lightblue',
-            'cadetblue',
-            'black'
-        ],
-        nodes=[0.0, 0.4, 0.47, 0.5, 0.53, 0.6, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'uppsala', [
-            'slategrey',
-            'goldenrod',
-            'antiquewhite',
-            'olivedrab',
-            'lawngreen',
-            'lightseagreen',
-            'paleturquoise'
-        ],
-        nodes=[0.0, 0.4, 0.46, 0.5, 0.58, 0.75, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'oslo', [
-            'black',
-            'saddlebrown',
-            'sienna',
-            'white',
-            'aliceblue',
-            'skyblue'
-        ],
-        nodes=[0.0, 0.48, 0.5, 0.51, 0.57, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'Lochinver', [
-            'floralwhite',
-            'antiquewhite',
-            'wheat',
-            'cornflowerblue',
-            'dodgerblue',
-            'powderblue'
-        ],
-        nodes=[0.0, 0.4, 0.45, 0.5, 0.65, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'tromso', [
-            'black',
-            'black',
-            'chocolate',
-            'ivory',
-            'sandybrown',
-            'black',
-            'black'
-        ],
-        nodes=[0.0, 0.3, 0.47, 0.5, 0.53, 0.7, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'osmort', [
-            'linen',
-            'linen',
-            'chocolate',
-            'black',
-            'sandybrown',
-            'linen',
-            'linen'
-        ],
-        nodes=[0.0, 0.4, 0.47, 0.5, 0.53, 0.6, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'irkoutsk', [
-            'darkkhaki',
-            'olive',
-            'dimgray',
-            'seashell',
-            'yellowgreen',
-            'darkolivegreen',
-            'olivedrab'
-        ],
-        nodes=[0.0, 0.4, 0.47, 0.5, 0.53, 0.6, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'krasnoiarsk', [
-            'darkgoldenrod',
-            'goldenrod',
-            'dimgray',
-            'cornsilk',
-            'gold',
-            'tan',
-            'burlywood'
-        ],
-        nodes=[0.0, 0.4, 0.47, 0.5, 0.53, 0.6, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'Rejkjavik', [
-            'rebeccapurple',
-            'indigo',
-            'darkorchid',
-            'gold',
-            'darkorange',
-            'orange',
-            'gold'
-        ],
-        nodes=[0.0, 0.3, 0.47, 0.5, 0.53, 0.7, 1.0])
-    createAndRegisterLinearSegmentedCmap(
-        'hs', [
-            'black',
-            '#100926',
-            'grey',
-            'gold',
-            'lemonchiffon',
-            'azure'
-        ],
-        nodes=[0.0, 0.2, 0.5, 0.6, 0.7, 1.0])
-
-
-def plot_color_gradients(cmap_category, cmap_list):
-    # Create figure and adjust figure height to number of colormaps
-    nrows = len(cmap_list)
-    figh = 0.35 + 0.15 + (nrows + (nrows-1)*0.1)*0.22
-    fig, axs = plt.subplots(nrows=nrows, figsize=(6.4, figh))
-    fig.subplots_adjust(top=1-.35/figh, bottom=.15/figh, left=0.2, right=0.99)
-
-    axs[0].set_title(cmap_category + ' colormaps', fontsize=14)
-    gradient = np.linspace(0, 1, 256)
-    gradient = np.vstack((gradient, gradient))
-
-    for ax, cmap_name in zip(axs, cmap_list):
-        ax.imshow(gradient, aspect='auto', cmap=cmap_name)
-        ax.text(-.01, .5, cmap_name, va='center', ha='right', fontsize=10,
-                transform=ax.transAxes)
-
-    # Turn off *all* ticks & spines, not just the ones with colormaps.
-    for ax in axs:
-        ax.set_axis_off()
-
-
-def import_pearsons_types():
-    species = {}
-    type = None
-    for each in pearson.keys():
-        type = pearson[each]
-        species[each] = [type['factors']['Da'],
-                         type['factors']['Db'],
-                         type['factors']['f'],
-                         type['factors']['k'],
-                         type['description']]
-    return species
-
-
-def setup_grid(rows, cols, blob_scale=0.1):
-    # 20x20 mesh point area located symmetrically around center to U(a) = 1/2,  V(b)=1/4.
-    # These conditions perturbed +/- 1% random noise to break the square symmetery
-    # grid is and two layers rows by cols numpy array filled first with 1
-    grid = np.ones((rows, cols, 2))
-    # the second layer is filled with 0
-    grid[:, :, 1] = 0.0
-
-    # here we seed the central part of the grid with randomized .5 on first layer and .25 on second layer values
-    DEV = 0.09
-    from_row = int((rows/2) - rows*blob_scale/2)
-    to_row = int((rows/2) + rows*blob_scale/2)
-    from_col = int((cols / 2) - cols * blob_scale / 2)
-    to_col = int((cols / 2) + cols * blob_scale / 2)
-
-    for i in range(from_row, to_row):
-        for j in range(int(np.random.uniform(1-DEV, 1+DEV)*from_col),
-                       int(np.random.uniform(1-DEV, 1+DEV)*to_col)
-                       ):
-            grid[i, j, 0] = 0.5
-            grid[i, j, 1] = 0.25
-
-    grid[from_row:to_row, from_col:to_col, :] = (
-                            (1+rand(to_row-from_row, to_col-from_col, 2) / 100)
-
-                            * grid[from_row:to_row, from_col:to_col, :]
-    )
-
-    return grid
-
+from gs_lib import (get_colormap, createColormaps,
+                    plot_color_gradients, import_pearsons_types, setup_grid)
 
 ################################################################################
 
@@ -477,6 +204,7 @@ class Canvas(app.Canvas):
         gl.GL_FRAGMENT_PRECISION_HIGH = 1
 
         # Dictionnary to map key commands to function
+        # All these functions will receive the calling event.
         self.keyactionDictionnary = {
             ' '    : self.initializeGrid,
             '/'    : self.switchReagent,
@@ -489,6 +217,8 @@ class Canvas(app.Canvas):
             '+'    : self.toggleGuidelines,
             '='    : self.resetFK
         }
+        # complete the dictionnary with keys of colormaps and species and give them
+        # their proper function
         for key in Canvas.colormapDictionnary.keys():
             self.keyactionDictionnary[key] = self.pickColorMap
         for key in Canvas.speciesDictionnary.keys():
--- shaders.py
+++ shaders.py
@@ -4,9 +4,12 @@
 # -----------------------------------------------------------------------------
 
 vertex_shader = """
+// model data
 attribute vec2 position;
 attribute vec2 texcoord;
+// Data (to be interpolated) that is passed on to the fragment shader
 varying vec2 highp v_texcoord;
+
 void main()
 {
     gl_Position = vec4(position, 0.0, 1.0);
@@ -236,6 +239,298 @@ void main(void)
         color = vec4(c, clamp(u, 0.0, 1.0), clamp(v, 0.0, 1.0));
     }
     gl_FragColor = color;
+}
+"""
+
+# This one is identical to the vertex_shader
+compute_vertex = """
+// model data
+attribute vec2 position;
+attribute vec2 texcoord;
+// Data (to be interpolated) that is passed on to the fragment shader
+varying vec2 v_texcoord;
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+    v_texcoord = texcoord;
+}
+"""
+
+# This one will/should become identical to the compute_fragment
+compute_fragment_2 = """
+uniform int pingpong;
+uniform float dx;                // horizontal distance between texels
+uniform float dy;                // vertical distance between texels
+uniform highp sampler2D texture; // u:= r or b following pinpong
+uniform highp vec2 brush;        // coordinates of mouse down
+uniform int brushtype;
+
+// Data coming from the vertex shader
+varying vec2 v_texcoord;
+
+void main()
+{
+    // Wheights for computing the diffusion Laplacian
+    float center = -1.0 * (sqrt(2.0) * 4.0 + 4.0);          // -1 * other weights
+    float diag   =  1.0;                                    // weight for diagonals
+    float neibor =  1.0 * sqrt(2.0);                        // weight for neighbours
+
+    vec2 highp p = v_texcoord;                              // center coordinates
+    vec2 highp c;
+    vec2 highp l;
+
+    if( pingpong == 0 ) {
+        c = texture2D(texture, p).rg;                       // central value
+                                                            // Compute Laplacian
+        l = ( texture2D(texture, p + vec2(-dx,-dy)).rg
+            + texture2D(texture, p + vec2( dx,-dy)).rg
+            + texture2D(texture, p + vec2(-dx, dy)).rg
+            + texture2D(texture, p + vec2( dx, dy)).rg) * diag
+            + ( texture2D(texture, p + vec2(-dx, 0.0)).rg
+            + texture2D(texture, p + vec2( dx, 0.0)).rg
+            + texture2D(texture, p + vec2(0.0,-dy)).rg
+            + texture2D(texture, p + vec2(0.0, dy)).rg) * neibor
+            + c * center;
+    } else {
+        c = texture2D(texture, p).ba;                       // central value
+                                                            // Compute Laplacian
+        l = ( texture2D(texture, p + vec2(-dx,-dy)).ba
+            + texture2D(texture, p + vec2( dx,-dy)).ba
+            + texture2D(texture, p + vec2(-dx, dy)).ba
+            + texture2D(texture, p + vec2( dx, dy)).ba) * diag
+            + ( texture2D(texture, p + vec2(-dx, 0.0)).ba
+            + texture2D(texture, p + vec2( dx, 0.0)).ba
+            + texture2D(texture, p + vec2(0.0,-dy)).ba
+            + texture2D(texture, p + vec2(0.0, dy)).ba) * neibor
+            + c * center;
+    }
+    float highp u = c.r;                                    // compute some temporary
+    float highp v = c.g;                                    // values which might save
+    float highp lu = l.r;                                   // a few GPU cycles
+    float highp lv = l.g;
+    float highp uvv = u * v * v;
+
+    // Currently values stubbed waiting for them to be properly imported.
+    float ru = 1.0;                                         // rate of diffusion of U
+    float rv = 0.5;                                         // rate of diffusion of V
+    float dd = 1.0;
+    float dt = 1.0;
+    float f = 0.026;
+    float k = 0.061;
+    float weight4 = sqrt(2.0) * 4.0 + 4.0;                  // Ratio of Diffusion U
+    float weight5 = sqrt(2.0) * 4.0 + 4.0;                  // Ratio of Diffusion V
+    // Gray-Scott equation diffusion+-reaction
+    float highp du = ru * lu / weight4 * dd - uvv + f * (1.0 - u);
+    float highp dv = rv * lv / weight5 * dd + uvv - (f + k) * v;
+    u += du * dt;
+    v += dv * dt;
+
+    // Manual mouse feed or kill
+    vec2 highp diff;
+    float dist;
+
+    // allow to force V concentrations locally
+    if (brush.x > 0.0) {
+        diff = (p - brush)/dx;
+        dist = dot(diff, diff);
+        if((brushtype == 1) && (dist < 3.0))
+            v = 0.5;
+        if((brushtype == 2) && (dist < 9.0))
+            v = 0.0;
+    }
+
+    vec4 highp color;
+    if( pingpong == 1 ) {
+        color = vec4(clamp(u, 0.0, 1.0), clamp(v, 0.0, 1.0), c);
+    } else {
+        color = vec4(c, clamp(u, 0.0, 1.0), clamp(v, 0.0, 1.0));
+    }
+    gl_FragColor = color;
+}
+"""
+
+render_3D_vertex = """
+// Scene transformations
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+uniform float dx;                // horizontal distance between texels
+uniform float dy;                // vertical distance between texels
+uniform int pingpong;
+uniform highp sampler2D texture; // u:= r or b following pinpong
+
+// Light model
+uniform vec4 u_color;
+
+// Original model data
+attribute vec3 position;
+attribute vec2 texcoord;
+attribute vec3 normal;
+attribute vec4 color;
+
+// Data (to be interpolated) that is passed on to the fragment shader
+varying vec3 v_position;
+varying vec3 v_normal;
+varying vec4 v_color;
+varying vec2 v_texcoord;
+
+void main()
+{
+    // Here position.z is received from texture
+    // and adjacent position.z are extracted too from texture
+    vec2 p = texcoord;
+    // read neightbor heights using an arbitrary small offset
+    // Offset should be 1 / width height
+    vec3 off = vec3(dx, dy, 0.0);
+    float c;
+    float hL;
+    float hR;
+    float hD;
+    float hU;
+
+    if( pingpong == 0 ) {
+        c = texture2DLod(texture, p, 0).g;                       // central value
+        hL = texture2DLod(texture, p - off.xz, 0).g;
+        hR = texture2DLod(texture, p + off.xz, 0).g;
+        hD = texture2DLod(texture, p - off.zy, 0).g;
+        hU = texture2DLod(texture, p + off.zy, 0).g;
+    } else {
+        c = texture2DLod(texture, p, 0).a;                       // central value
+        hL = texture2DLod(texture, p - off.xz, 0).a;
+        hR = texture2DLod(texture, p + off.xz, 0).a;
+        hD = texture2DLod(texture, p - off.zy, 0).a;
+        hU = texture2DLod(texture, p + off.zy, 0).a;
+    }
+    vec3 position2 = vec3(position.x, position.y, c);
+
+    // Perform the model and view transformations on the vertex and pass this
+    // location to the fragment shader.
+    v_position = vec3(u_view * u_model * vec4(position2, 1.0));
+
+    // Here since position has been realtime modified, normals have to be computed again
+    vec3 N;
+    N.x = (hL - hR)/dx;
+    N.y = (hD - hU)/dy;
+    N.z = 2.0;
+    N = normalize(N);
+
+    // Perform the model and view transformations on the vertex's normal vector
+    // and pass this normal vector to the fragment shader.
+    v_normal = vec3(u_view * u_model * vec4(N, 0.0));
+
+    // Pass the vertex's color to the fragment shader.
+    // v_color = vec4(c, c, c, 0);
+    v_texcoord = texcoord;
+
+    // Transform the location of the vertex for the rest of the graphics pipeline
+    gl_Position = u_projection * u_view * u_model * vec4(position2, 1.0);
+}
+"""
+
+render_3D_fragment = """
+uniform int pingpong;
+
+// Light model
+uniform vec3 u_light_position;
+uniform vec3 u_light_intensity;
+uniform float u_Shininess;
+uniform vec3 u_Ambient_color;
+uniform float c1;
+uniform float c2;
+uniform float c3;
+
+uniform highp sampler2D texture; // u:= r or b following pinpong
+
+// Data coming from the vertex shader
+varying vec3 v_position;
+varying vec3 v_normal;
+varying vec2 v_texcoord;
+
+void main()
+{
+    vec3 to_light;
+    vec3 vertex_normal;
+    vec3 reflection;
+    vec3 to_camera;
+    float cos_angle;
+    vec3 diffuse_color;
+    vec3 specular_color;
+    vec3 ambient_color;
+    vec3 color;
+    float light_distance;
+    float attenuation;
+
+    float u;
+    int reagent = 0;
+    // pingpong between layers and choice of reagent
+    if(pingpong == 0) {
+        if(reagent == 1){
+            u = texture2D(texture, v_texcoord).r;
+        } else {
+            u = texture2D(texture, v_texcoord).g;
+        }
+    } else {
+        if(reagent == 1){
+            u = texture2D(texture, v_texcoord).b;
+        } else {
+            u = texture2D(texture, v_texcoord).a;
+        }
+    }
+    vec4 v_color = vec4(u, u, u, 0);
+
+    // Calculate the ambient color as a percentage of the surface color
+    ambient_color = u_Ambient_color * vec3(v_color);
+
+    // Calculate a vector from the fragment location to the light source
+    to_light = u_light_position - v_position;
+
+    // while computing this vector, let's compute its length and the attenuation
+    // due to it before normalizing it
+    light_distance = length(to_light);
+    attenuation = 1.0/(c1 + c2*light_distance + c3*light_distance*light_distance);
+    to_light = normalize( to_light );
+
+    // The vertex's normal vector is being interpolated across the primitive
+    // which can make it un-normalized. So normalize the vertex's normal vector.
+    vertex_normal = normalize( v_normal );
+
+    // Calculate the cosine of the angle between the vertex's normal vector
+    // and the vector going to the light.
+    cos_angle = dot(vertex_normal, to_light);
+    cos_angle = clamp(cos_angle, 0.0, 1.0);
+
+    // Scale the color of this fragment based on its angle to the light.
+    diffuse_color = vec3(v_color) * cos_angle;
+
+    // Calculate the reflection vector
+    reflection = 2.0 * dot(vertex_normal,to_light) * vertex_normal - to_light;
+
+    // Calculate a vector from the fragment location to the camera.
+    // The camera is at the origin, so negating the vertex location gives the vector
+    to_camera = -1.0 * v_position;
+
+    // Calculate the cosine of the angle between the reflection vector
+    // and the vector going to the camera.
+    reflection = normalize( reflection );
+    to_camera = normalize( to_camera );
+    cos_angle = dot(reflection, to_camera);
+    cos_angle = clamp(cos_angle, 0.0, 1.0);
+    cos_angle = pow(cos_angle, u_Shininess);
+
+    // The specular color is from the light source, not the object
+    if (cos_angle > 0.0) {
+    specular_color = u_light_intensity * cos_angle;
+    diffuse_color = diffuse_color * (1.0 - cos_angle);
+    } else {
+    specular_color = vec3(0.0, 0.0, 0.0);
+    }
+
+    // don't really know on which part of the light sources should the attenuation play
+    // Maybe not on the ambient_color?
+    color = ambient_color + attenuation * (diffuse_color + specular_color);
+
+    gl_FragColor = vec4(color, v_color.a);
 }
 """
 
